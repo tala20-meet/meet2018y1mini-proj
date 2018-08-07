a31b6feecc8dd1c88ff9a4b87773f26c1d3298f4
tala20-meet <tala20@meet.mit.edu>
2018-08-07 09:58:59 -0400

    Stopping here

--- snakegame.py
+++ snakegame.py
@@ -10,7 +10,7 @@ turtle.setup(SIZE_X, SIZE_Y) #Curious? It's the turtle window
 turtle.penup()
 
 SQUARE_SIZE = 20
-START_LENGTH = 6
+START_LENGTH = 10
 
 #Initialize lists
 pos_list = []
@@ -34,17 +34,131 @@ for start in range(START_LENGTH) :
 
     #Add SQUARE_SIZE to x_pos. Where does x_pos point to now?    
     # You're RIGHT!
-    x_pos+=SQUARE_LENGTH
+    x_pos+=SQUARE_SIZE
 
     my_pos=(x_pos,y_pos) #Store position variables in a tuple
     snake.goto(x_pos,y_pos) #Move snake to new (x,y)
    
     #Append the new position tuple to pos_list
-    pos_list.append(x_pos,y_pos) 
+    pos_list.append(my_pos) 
 
     #Save the stamp ID! You'll need to erase it later. Then append
     # it to stamp_list.             
-     = snake.stamp()
-    ______.append(_____________)
+    stamp_ID= snake.stamp()
+    stamp_list.append(stamp_ID)
+
+UP_ARROW = "Up" #Make sure you pay attention to upper and lower 
+                #case
+LEFT_ARROW = "Left" #Pay attention to upper and lower case
+DOWN_ARROW = "Down" #Pay attention to upper and lower case
+RIGHT_ARROW = "Right" #Pay attention to upper and lower case
+TIME_STEP = 100 #Update snake position after this many 
+                    #milliseconds
+SPACEBAR = "space" # Careful, it's not supposed to be capitalized!
+
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
+
+direction = UP
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
+
+def up():
+    direction=UP #Change direction to up
+    print("You pressed the up key!")
+
+    direction = DOWN
+def down():
+    direction=DOWN
+    print("You pressed the down key!")
+
+direction = RIGHT
+def right():
+   direction=RIGHT
+   print("You pressed the right key!")
+
+direction = LEFT
+def left():
+   direction=LEFT #Change direction to up
+   print("You pressed the left key!")
+
+turtle.onkeypress(up, UP_ARROW)
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(right, RIGHT_ARROW)
+turtle.onkeypress(left, LEFT_ARROW)
+
+turtle.listen()
+
+def move_snake():
+    my_pos = snake.pos()
+    x_pos = my_pos[0]
+    y_pos = my_pos[1]
+    turtle.ontimer(move_snake,TIME_STEP)
+    
+    if direction==RIGHT:
+        snake.goto(x_pos + SQUARE_SIZE, y_pos)
+        print("You moved right!")
+    elif direction==LEFT:
+        snake.goto(x_pos - SQUARE_SIZE, y_pos)
+        print("You moved left!")
+    elif direction==UP:
+        snake.goto(x_pos,y_pos+SQUARE_SIZE)
+        print("You moved up!")
+    elif direction==DOWN:
+        snake.goto(x_pos,y_pos-SQUARE_SIZE)
+        print("You moved down!")
+
+    my_pos=snake.pos() 
+    pos_list.append(my_pos)
+    new_stamp = snake.stamp()
+    stamp_list.append(new_stamp)
+    ######## SPECIAL PLACE - Remember it for Part 5
+    #pop zeroth element in pos_list to get rid of last the last 
+    #piece of the tail
+    old_stamp = stamp_list.pop(0)
+    snake.clearstamp(old_stamp)
+    pos_list.pop(0)
+    
+    #Add new lines to the end of the function
+    #Grab position of snake
+    new_pos = snake.pos()
+    new_x_pos = new_pos[0]
+    new_y_pos = new_pos[1]
+
+    # The next three lines check if the snake is hitting the 
+    # right edge.
+    if new_x_pos >= RIGHT_EDGE:
+        print("You hit the right edge! Game over!")
+        quit()
+    if new_x_pos <= LEFT_EDGE:
+        print("You hit the left edge! Game over!")
+        quit()
+    if new_y_pos >= UP_EDGE:
+        print("You hit the up edge! Game over!")
+        quit()
+    if new_y_pos <= DOWN_EDGE:
+        print("You hit the down edge! Game over!")
+        quit() 
+move_snake()
+
+
+
+    
+
+    
+
+        
+
+
+
+    
+
+
+
+    
 
 
