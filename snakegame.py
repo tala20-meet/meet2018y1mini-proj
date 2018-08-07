d707908ee4834c72410eed9b07c884076dca8b47
tala20-meet <tala20@meet.mit.edu>
2018-08-07 07:28:10 -0400

    Stopping here

---
+++ snakegame.py
@@ -0,0 +1,50 @@
+import turtle
+import random #We'll need this later in the lab
+
+turtle.tracer(1,0) #This helps the turtle move more smoothly
+
+SIZE_X=800
+SIZE_Y=500
+turtle.setup(SIZE_X, SIZE_Y) #Curious? It's the turtle window  
+                             #size. 
+turtle.penup()
+
+SQUARE_SIZE = 20
+START_LENGTH = 6
+
+#Initialize lists
+pos_list = []
+stamp_list = []
+food_pos = []
+food_stamps = []
+
+#Set up positions (x,y) of boxes that make up the snake
+snake = turtle.clone()
+snake.shape("square")
+
+#Hide the turtle object (it's an arrow - we don't need to see it)
+turtle.hideturtle()
+
+#Draw a snake at the start of the game with a for loop
+#for loop should use range() and count up to the number of pieces
+#in the snake (i.e. START_LENGTH)
+for start in range(START_LENGTH) :
+    x_pos=snake.pos()[0]#Get x-position with snake.pos()[0]
+    y_pos=snake.pos()[1]
+
+    #Add SQUARE_SIZE to x_pos. Where does x_pos point to now?    
+    # You're RIGHT!
+    x_pos+=SQUARE_LENGTH
+
+    my_pos=(x_pos,y_pos) #Store position variables in a tuple
+    snake.goto(x_pos,y_pos) #Move snake to new (x,y)
+   
+    #Append the new position tuple to pos_list
+    pos_list.append(x_pos,y_pos) 
+
+    #Save the stamp ID! You'll need to erase it later. Then append
+    # it to stamp_list.             
+     = snake.stamp()
+    ______.append(_____________)
+
+
